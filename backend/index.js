4d32489d0550599dc4a83f65355a04f5237f15cd
Divya Prakash <89380519+divyap3009@users.noreply.github.com>
2024-10-21 21:13:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use('/players', playerRoutes);
 app.use('/teams', teamRoutes);
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
